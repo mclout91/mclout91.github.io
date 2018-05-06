9f0e86b7114eff0fe142523c51192cbfe913368f
macclouting@gmail.com <macclouting@gmail.com>
2018-05-06 12:52:24 +0100

    widescreen sketch

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
+
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(window.innerWidth, 100);
 }
 
 function draw() {
+
   if (mouseIsPressed) {
-    fill(0);
+    fill(0,0,0);
   } else {
-    fill(255);
+    fill(255,255,255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, 1, 1);
+  //background(220, 180, 200);
 }
